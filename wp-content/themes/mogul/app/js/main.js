1a213ce332bb8411de311624a12414e0a2700013
Exsy <info@exsy.com.ua>
2017-08-03 21:19:03 +0300

    Added contact page and modal form with pixel perfect and responsive.

--- wp-content/themes/mogul/app/js/main.js
+++ wp-content/themes/mogul/app/js/main.js
@@ -1,13 +1,38 @@
 ;(function () {
 
-    var $body = $('body');
+    var $body = $('body'),
+        $siteMain = $('.site-main'),
+        subjectTitle = $siteMain.find('h2').first().text(),
+        $modalWrap = $('.modal-wrap');
 
     $(document).ready(function($) {
 
-        //-----------Show mobile nav menu
+        /*-----------Show mobile nav menu*/
             $('.hamburger').click(function(event) {
                 event.preventDefault();
                 $body.toggleClass('js-nav-menu-active');
+            });
+        /*------------Show, close modal*/
+            $(document).on('click', '.js-show-modal', function(event) {
+                event.preventDefault();
+                 $modalWrap.find('.main-form__title').text(subjectTitle);
+                 $modalWrap.find('.field-subject').val(subjectTitle);
+                openCloseModal();
+            }); // end click
+
+             $modalWrap.click(function(e){
+                openCloseModal();
+            }).children().click(function(e){        
+                e.stopPropagation();   
+            }); // end click
+
+            $('.modal__close').click(function(event) {
+                event.preventDefault();
+                openCloseModal();
+            }); // end click
+        /*------------Set main form subject to input value*/
+            $(document).on('click', '.main-form__btn', function() {
+                $siteMain.find('.field-subject').val(subjectTitle);
             });
     
     }); // end ready
@@ -35,5 +60,15 @@
 
             return ( totalHeight <= pagePos + $window.height() && elemPos >= pagePos);
           }
+    /*---------Open close modal*/
+        function openCloseModal() {
+            if( !$body.hasClass('js-modal-open') ) {
+                $body.addClass('js-modal-open');
+                 $modalWrap.addClass('js-modal-open');
+            } else {
+                $body.removeClass('js-modal-open');
+                 $modalWrap.removeClass('js-modal-open');
+            }
+        }
 
 })();
